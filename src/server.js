9b3f4ffa54360e9d1b20f3892818addeac335c53
Paul DeSimone <pjd3949@rit.edu>
2017-10-18 17:55:46 -0400

    Fixed Circle CI Errors

--- src/server.js
+++ src/server.js
@@ -4,20 +4,25 @@ const socketio = require('socket.io');
 const url = require('url');
 const path = require('path');
 
+// port to host on
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// reference to index.html
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 
+// Fetches all the necessary files
 const onRequest = (request, response) => {
   const pathname = url.parse(request.url).pathname;
   const ext = path.extname(pathname);
+    
+  // writes the file based on the file extensions to import all external sources
   if (ext) {
     if (ext === '.css') {
       response.writeHead(200, { 'Content-Type': 'text/css' });
     } else if (ext === '.js') {
       response.writeHead(200, { 'Content-Type': 'text/javascript' });
     }
-    response.write(fs.readFileSync(path.join(__dirname, pathname), 'utf8'));
+    response.write(fs.readFileSync(path.join(`${__dirname}/../client`, pathname), 'utf8'));
   } else {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(index);
@@ -25,16 +30,17 @@ const onRequest = (request, response) => {
   response.end();
 };
 
+// Creates the server
 const app = http.createServer(onRequest).listen(port);
-
-console.log(`Listening on 127.0.0.1: ${port}`);
 
 // pass in the http server into socketio and grab the websocket server as io
 const io = socketio(app);
 
+// Holds number of users and number of users readied up
 let users = 0;
 let ready = 0;
 
+// Used to get a random color
 // https://stackoverflow.com/questions/1484506/random-color-generator
 const getRandomColor = () => {
   const letters = '0123456789ABCDEF';
@@ -47,6 +53,7 @@ const getRandomColor = () => {
   return color;
 };
 
+// Hooks up socket events and calls
 const addObject = (sock) => {
   const socket = sock;
 
@@ -75,11 +82,9 @@ const addObject = (sock) => {
   });
 };
 
+// Joins the room, updates users, call hookup to socket events
 io.sockets.on('connection', (socket) => {
-  console.log('connected!');
-
   socket.join('room1');
-
   users++;
   io.sockets.in('room1').emit('newConnection', users);
   addObject(socket);
