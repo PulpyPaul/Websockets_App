3e3f68f5519b6e911d12a6a9870d27252a1a5e14
Paul DeSimone <pjd3949@rit.edu>
2017-10-18 12:52:37 -0400

    Added organized html structure

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 const http = require('http');
 const fs = require('fs');
 const socketio = require('socket.io');
+const url = require('url');
+const path = require('path');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 
 const onRequest = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
+  const pathname = url.parse(request.url).pathname;
+  const ext = path.extname(pathname);
+  if (ext) {
+    if (ext === '.css') {
+      response.writeHead(200, { 'Content-Type': 'text/css' });
+    } else if (ext === '.js') {
+      response.writeHead(200, { 'Content-Type': 'text/javascript' });
+    }
+    response.write(fs.readFileSync(path.join(__dirname, pathname), 'utf8'));
+  } else {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(index);
+  }
   response.end();
 };
 
@@ -21,6 +34,18 @@ const io = socketio(app);
 
 let users = 0;
 let ready = 0;
+
+// https://stackoverflow.com/questions/1484506/random-color-generator
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
 
 const addObject = (sock) => {
   const socket = sock;
@@ -44,9 +69,9 @@ const addObject = (sock) => {
     ready++;
     io.sockets.in('room1').emit('updateReadyStatus', ready);
   });
-    
+
   socket.on('startRound', () => {
-     io.sockets.in('room1').emit('restartRound', getRandomColor()); 
+    io.sockets.in('room1').emit('restartRound', getRandomColor());
   });
 };
 
@@ -59,16 +84,4 @@ io.sockets.on('connection', (socket) => {
   io.sockets.in('room1').emit('newConnection', users);
   addObject(socket);
 });
-
-// https://stackoverflow.com/questions/1484506/random-color-generator
-const getRandomColor = () => {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-            
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    
-    return color;
-}
 
